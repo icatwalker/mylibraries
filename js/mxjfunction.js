650d76a57f5aa8b501ed46c4fd3dc70071dc654d
icatwalker <m804396134@gmail.com>
2017-07-17 17:24:55 +0800

    111

--- js/mxjfunction.js
+++ js/mxjfunction.js
@@ -1,12 +1,11 @@
 ﻿Function.prototype.addFunction = function (name, fn) {// 基于jquery 引用时候放到 jquery 后
     this.prototype[name] = fn;
     return this;//链式添加方法；
-}
+};
 var Mxjfn = new Function();
 Mxjfn
     .addFunction("showItem", function (selector) {//点击 有样式变化， 下拉框 
         $(selector).on("click", function () {
-          
             $(this)
                 .toggleClass("active")
                 .next().toggle("slow");
@@ -15,11 +14,11 @@ Mxjfn
     })
     .addFunction("key_home", function (selector, key) {//触发的按键，和 按键code 元素中要有onclick
         $("body").bind('keypress', function (event) {
-            if (event.keyCode == key) { $(selector).click() };
+            if (event.keyCode == key) { $(selector).click() }
         });
         return this;
     })
-    .addFunction("selectAll", function (faSel,childSel) {// 点击复选框 ， 用的时候复制一份 faSel有唯一标示 
+    .addFunction("selectAll", function (faSel,childSel) {// 点击复选框 ， 用的时候复制一份 faSel有唯一标示 ,子元素有统一class 或标示
         function doCheck() {
             var ch = $(childSel);
             if ($(faSel).prop("checked")==true) {
@@ -34,7 +33,7 @@ Mxjfn
         }
         $(faSel).click(function () {
             doCheck();
-        })
+        });
         return this;
     })
 
